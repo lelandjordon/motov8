8e54a5848380bb6258ae79381a8b740ec0053ef5
Leland Jordon <lelandjordon@mac.com>
2016-04-25 13:33:31 -0400

    created db. May have done it wrong.

--- db/migrate/20160424221618_create_posts.rb
+++ db/migrate/20160424221618_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+      t.string :body
+      t.string :author
       t.timestamps null: false
     end
   end
