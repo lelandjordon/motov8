541dc5f0cb580ff7a984d32647e685764ac55ed4
Leland Jordon <lelandjordon@mac.com>
2016-04-25 19:35:04 -0400

    added title to comments correctly this time.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425185309) do
+ActiveRecord::Schema.define(version: 20160425233345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160425185309) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
   create_table "posts", force: :cascade do |t|
