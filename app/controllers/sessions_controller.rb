455c435b0d24cec0866a7b966ba59adf53b551ad
Leland Jordon <lelandjordon@mac.com>
2016-04-25 16:11:53 -0400

    working on author authorizations.  Now considering renaming Authors to Users.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    # does the author exist with the email provided?
+    author = Author.find_by(email: params[:email])
+
+    if author
+      # does their password match?
+      if author.authenticate(params[:password])
+        # if so, log them in
+        session[:author_id] = author.id
+        flash[:notice] = "Logged in as #{author.email}!"
+        redirect_to artists_path
+      else
+        # else, send them back to the log in form
+        render :new and return
+      end
+    else
+      # incorrect email (no author with that email)
+      # else, send them back to the log in form
+      render :new and return
+    end
+
+  end
+
+  def destroy
+    session[:author_id] = nil
+    redirect_to artists_path, notice: "Signed out!"
+  end
+
+end
